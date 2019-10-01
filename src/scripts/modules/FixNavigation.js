05ef5fa0643185603c21bbaeeea4fdd9382f7da7
aotsuka-pnrm <aotsuka@pnrm.co.jp>
2019-10-01 16:22:15 +0900

    feat: ナビ挙動実装

--- src/scripts/modules/FixNavigation.js
+++ src/scripts/modules/FixNavigation.js
@@ -6,16 +6,39 @@ export default class FixNav {
     this.addEvents()
   }
   addEvents() {
+    const ua_2 = navigator.userAgent
+    if (!ua_2.match(/(iPhone|iPad|iPod|Android)/i)) {
+      this.elem.classList.add('is-hidden')
+    }
     window.addEventListener('scroll', this.onScroll.bind(this))
   }
 
   onScroll() {
     const top = this.elem.getBoundingClientRect().top
+    const win_top = document.body.getBoundingClientRect().top
+    const winTop = window.pageYOffset
     const relativeHeight = this.elem.getBoundingClientRect().height * -1
-    if (top < 0) {
-      this.elem.children[0].classList.add('is-fixed')
-    } else if (top > relativeHeight) {
-      this.elem.children[0].classList.remove('is-fixed')
+    const ua = navigator.userAgent
+    // console.log(this.elem.getBoundingClientRect().top)
+    // console.log(this.elem.getBoundingClientRect().win_top)
+    // console.log(this.elem.getBoundingClientRect().height * -1)
+    // console.log(window.pageYOffset)
+    if (ua.match(/(iPhone|iPad|iPod|Android)/i)) {
+      if (top < 0) {
+        this.elem.children[0].classList.add('is-fixed')
+      } else if (top > relativeHeight) {
+        this.elem.children[0].classList.remove('is-fixed')
+      }
+    } else {
+      // if (top < -1200) {
+      if (winTop > 1080) {
+        this.elem.classList.remove('is-hidden')
+        this.elem.children[0].classList.add('is-fixed')
+        // } else if (140 > relativeHeight) {
+      } else {
+        this.elem.classList.add('is-hidden')
+        this.elem.children[0].classList.remove('is-fixed')
+      }
     }
   }
 }
