743832379d32fab2308e9117d05077aa5e55d644
DaiAotsuka <daiaot@gmail.com>
2019-10-15 01:16:34 +0900

    feat: スプラッシュ調整

--- src/scripts/modules/Splash.js
+++ src/scripts/modules/Splash.js
@@ -6,20 +6,28 @@ export default class Splash {
     this.body = document.body
     this.isSplash = elem.classList.contains('p-splash') ? true : false
     this.bg = elem.querySelector('[data-module-loading="bg"]')
+    this.msg = elem.querySelectorAll('.p-loading__msg')
+    // console.log('---- this.msg')
+    // console.log(this.msg)
     this.heightAdjust()
     this.bindEvents()
   }
 
   heightAdjust() {
     let target = document.getElementsByClassName('p-splash')
-    if (this.isSplash === true && window.devicePixelRatio === 3 &&
+    if (
+      this.isSplash === true &&
+      window.devicePixelRatio === 3 &&
       (window.screen.width === 375 || window.screen.height === 375) &&
-      /iPhone/.test(window.navigator.userAgent)) {
+      /iPhone/.test(window.navigator.userAgent)
+    ) {
       target[0].style.height = 'calc(100vh - 110px)'
-    } else if (this.isSplash === true && navigator.userAgent.match(/(iPhone|iPod)/i)) {
+    } else if (
+      this.isSplash === true &&
+      navigator.userAgent.match(/(iPhone|iPod)/i)
+    ) {
       target[0].style.height = 'calc(100vh - 75px)'
-    }
-    else if (
+    } else if (
       this.isSplash === true &&
       navigator.userAgent.match(/(Android)/i)
     ) {
@@ -43,10 +51,17 @@ export default class Splash {
     })
   }
 
-
-
   topSplash() {
     return new Promise(() => {
+      // TweenMax.set(this.body, { css: { overflow: 'hidden' } })
+      // TweenMax.set(this.body, { css: { posison: 'fixde' } })
+      // TweenMax.set(this.body, { css: { top: '0px' } })
+
+      TweenMax.set(this.msg, 2.0, {
+        ease: Expo.easeInOut,
+        opacity: 1,
+      })
+
       TweenMax.set(this.bg, {
         opacity: 1,
         zIndex: 110,
@@ -59,6 +74,13 @@ export default class Splash {
         zIndex: 0,
       })
 
+      // TweenMax.set(this.msg, {
+      //   opacity: 0,
+      // })
+      // TweenMax.to(this.msg, 2.0, {
+      //   ease: Expo.easeInOut,
+      //   opacity: 1,
+      // })
     })
   }
 
@@ -77,10 +99,30 @@ export default class Splash {
 
     // await TweenMax.to(this.bg, 0.01, { display: 'none' })
     // await TweenMax.to(this.bg, 0.8, { display: 'none' })
-    this.topSplash()
+
+    // TweenMax.set(this.body, {
+    //   overflow: 'hidden',
+    //   position: 'fixed',
+    //   top: '0px',
+    // })
+    // await this.topSplash()
+    await this.topSplash()
+
+    TweenMax.set(this.body, {
+      overflow: '',
+      position: '',
+      top: '',
+    })
+
     // await TweenMax.set(this.body, { css: { overflow: 'scroll' } })
-    await TweenMax.set(this.body, { css: { overflowX: 'hidden' } })
-    await TweenMax.set(this.body, { css: { width: '100vw' } })
+
+    // await TweenMax.set(this.body, { css: { overflowX: 'hidden' } })
+    // await TweenMax.set(this.body, { css: { width: '100vw' } })
+
+    // await TweenMax.set(this.body, { css: { overflow: '' } })
+    // await TweenMax.set(this.body, { css: { posison: '' } })
+    // await TweenMax.set(this.body, { css: { top: '' } })
+
     // await TweenMax.set(this.kvImage, { css: { opacity: '1' } })
     // await this.slideOutVideoMask()
     // await emit.ev.emit('pageLoaded', true)
