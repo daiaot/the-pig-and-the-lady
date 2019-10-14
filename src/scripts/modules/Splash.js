c832d4ebc826edbdc4fb2dc64310bb2f227d1537
DaiAotsuka <daiaot@gmail.com>
2019-10-15 02:04:13 +0900

    fix: splashモジュールを元に戻す

--- src/scripts/modules/Splash.js
+++ src/scripts/modules/Splash.js
@@ -6,9 +6,6 @@ export default class Splash {
     this.body = document.body
     this.isSplash = elem.classList.contains('p-splash') ? true : false
     this.bg = elem.querySelector('[data-module-loading="bg"]')
-    this.msg = elem.querySelectorAll('.p-loading__msg')
-    // console.log('---- this.msg')
-    // console.log(this.msg)
     this.heightAdjust()
     this.bindEvents()
   }
@@ -53,15 +50,6 @@ export default class Splash {
 
   topSplash() {
     return new Promise(() => {
-      // TweenMax.set(this.body, { css: { overflow: 'hidden' } })
-      // TweenMax.set(this.body, { css: { posison: 'fixde' } })
-      // TweenMax.set(this.body, { css: { top: '0px' } })
-
-      TweenMax.set(this.msg, 2.0, {
-        ease: Expo.easeInOut,
-        opacity: 1,
-      })
-
       TweenMax.set(this.bg, {
         opacity: 1,
         zIndex: 110,
@@ -73,60 +61,20 @@ export default class Splash {
         opacity: 0,
         zIndex: 0,
       })
-
-      // TweenMax.set(this.msg, {
-      //   opacity: 0,
-      // })
-      // TweenMax.to(this.msg, 2.0, {
-      //   ease: Expo.easeInOut,
-      //   opacity: 1,
-      // })
     })
   }
-
-  // async transitionStart() {
-  //   await this.prepareSlideOutAnimation()
-  //   await this.slideInVideoMask()
-  //   await this.locationHref()
-  //   // await this.topSplash()
-  // }
-
   async animate() {
     // const ua = navigator.userAgent
     // if (ua.match(/(iPhone|iPad|iPod|Android)/i)) {
     // } else {
     // }
-
-    // await TweenMax.to(this.bg, 0.01, { display: 'none' })
-    // await TweenMax.to(this.bg, 0.8, { display: 'none' })
-
-    // TweenMax.set(this.body, {
-    //   overflow: 'hidden',
-    //   position: 'fixed',
-    //   top: '0px',
-    // })
-    // await this.topSplash()
-    await this.topSplash()
-
-    TweenMax.set(this.body, {
-      overflow: '',
-      position: '',
-      top: '',
-    })
-
+    this.topSplash()
     // await TweenMax.set(this.body, { css: { overflow: 'scroll' } })
-
     // await TweenMax.set(this.body, { css: { overflowX: 'hidden' } })
     // await TweenMax.set(this.body, { css: { width: '100vw' } })
 
     // await TweenMax.set(this.body, { css: { overflow: '' } })
     // await TweenMax.set(this.body, { css: { posison: '' } })
     // await TweenMax.set(this.body, { css: { top: '' } })
-
-    // await TweenMax.set(this.kvImage, { css: { opacity: '1' } })
-    // await this.slideOutVideoMask()
-    // await emit.ev.emit('pageLoaded', true)
-    // await this.slideInNavigation()
-    // await this.fadeInNavLinks()
   }
 }
