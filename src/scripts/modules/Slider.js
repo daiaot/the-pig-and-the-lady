c345fa09163f8a0cca73229571aacaa29faedc58
aotsuka-pnrm <aotsuka@pnrm.co.jp>
2019-11-26 14:03:42 +0900

    fix: ブレイクポイント戻す

--- src/scripts/modules/Slider.js
+++ src/scripts/modules/Slider.js
@@ -28,7 +28,7 @@ export default class Slider {
       // cssEase: 'ease',
       responsive: [
         {
-          breakpoint: 768,
+          breakpoint: 795,
           settings: {
             centerPadding: '20px',
             variableWidth: false,
