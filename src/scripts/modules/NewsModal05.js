00883c51928a75d84d8beaa93b9155f0e0f77a7c
aotsuka-pnrm <aotsuka@pnrm.co.jp>
2019-12-16 15:49:34 +0900

    feat: news追加

---
+++ src/scripts/modules/NewsModal05.js
@@ -0,0 +1,57 @@
+
+import $ from 'jquery'
+import Swiper from 'swiper/dist/js/swiper'
+
+export default class NewsModal05 {
+  constructor(elem) {
+    this.elem = elem
+    this.init()
+  }
+
+  init() {
+    // const modal = $('#js-galleryModal')
+    // const modalClose = $('#js-galleryModalClose')
+    // const modalOverlay = $('#js-galleryModalOverlay')
+
+    const modal = $('#js-modal05')
+    const modalClose = $('#js-modal05Close')
+    const modalOverlay = $('#js-modal05Overlay')
+    const open = 'is-open'
+
+    // swiper
+    // const modalSwiper = new Swiper('#js-galleryModalSwiper', {
+    const modalSwiper = new Swiper('#js-modal05Swiper', {
+      // loop: true,
+      // spaceBetween: 30,
+      simulateTouch: false,
+      // noSwiping: true,
+    })
+
+    // modalを開く対象の指定
+    // $('.gallery-content__item a').on('click', function(e) {
+    $('.gallery-content__item_05').on('click', function(e) {
+      e.preventDefault()
+
+      const slideNum = $(this).data('slide')
+      // console.log('slideNum : ' + slideNum)
+      // console.log(slideNum)
+      modalSwiper.slideTo(slideNum, 0)
+      if (!modal.hasClass(open)) {
+        modal.css('visibility', 'visible').addClass(open)
+        $('html').css('overflow', 'hidden')
+      }
+      // console.log('modalSwiper : ' + modalSwiper.realIndex);
+    })
+
+    // 以下非表示系
+    modalClose.on('click', function() {
+      modal.css('visibility', 'hidden').removeClass(open)
+      $('html').css('overflow', 'visible')
+    })
+
+    modalOverlay.on('click', function() {
+      modal.css('visibility', 'hidden').removeClass(open)
+      $('html').css('overflow', 'visible')
+    })
+  }
+}
